c7610b105644aafa38583c0dd9014209d1bbe06d
Augusto Pagnossim Frigo <frigo.augusto@gmail.com>
2021-08-12 16:07:43 -0300

    interface fix

--- core/src/com/uga/game/Entity.java
+++ core/src/com/uga/game/Entity.java
@@ -1,8 +1,11 @@
 package com.uga.game;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
 public interface Entity {
 
 
-    public void render();
+    public void render(Rectangle teste, SpriteBatch batch);
     public void setCharacter();
 }
